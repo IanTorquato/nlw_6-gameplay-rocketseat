266d8fede18df6a9456d3598da476129b9daf0ab
Ian Torquato <iantorquato2@gmail.com>
2021-12-11 17:09:44 -0300

    Changing button type

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
 import DiscordImg from '@assets/discord.png';
 
 import { styles } from './styles';
 
-interface Props extends TouchableOpacityProps {
+interface Props extends RectButtonProps {
   title: string;
 }
 
 export function ButtonIcon({ title, ...props }: Props) {
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.8} {...props}>
+    <RectButton style={styles.container} {...props}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon}></Image>
       </View>
 
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
