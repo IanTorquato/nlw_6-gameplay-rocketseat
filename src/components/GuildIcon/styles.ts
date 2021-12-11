0778d01e5ce43441495150e0f20f0adc3e1a3750
Ian Torquato <iantorquato2@gmail.com>
2021-12-11 20:27:54 -0300

    Adding GuildIcon component

---
+++ src/components/GuildIcon/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  image: {
+    borderRadius: 8,
+    height: 64,
+    marginRight: 24,
+    width: 64,
+  },
+});
