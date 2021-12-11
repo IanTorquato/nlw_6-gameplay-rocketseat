5f3900f4f5d84b6e8ed54b2034f97bf1ee58a70b
Ian Torquato <iantorquato2@gmail.com>
2021-12-11 20:27:37 -0300

    Adding ListDivider component

---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+import { theme } from '@global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'flex-end',
+    backgroundColor: theme.colors.secondary40,
+    height: 1,
+    marginVertical: 16,
+    width: '80%',
+  },
+});
