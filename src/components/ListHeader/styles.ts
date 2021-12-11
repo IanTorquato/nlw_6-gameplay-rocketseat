29257e2a08f06e8dc29b6b9cd3bcbdf302285774
Ian Torquato <iantorquato2@gmail.com>
2021-12-11 20:28:35 -0300

    Adding ListHeader component

---
+++ src/components/ListHeader/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+import { theme } from '@global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    width: '100%',
+  },
+  title: {
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.titleBold,
+    fontSize: 18,
+  },
+  subtitle: {
+    color: theme.colors.highlight,
+    fontFamily: theme.fonts.textRegular,
+    fontSize: 13,
+  },
+});
